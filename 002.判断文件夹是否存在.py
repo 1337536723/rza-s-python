51d5be90e3f3651b1f2dd9c5bf008aa95a9d83a7
RenZhiai <404261318@qq.com>
2016-07-13 19:24:21 +0800

    Update and rename 002.判断文件夹是否存在 to 002.判断文件夹是否存在.py

--- 002.判断文件夹是否存在.py
+++ 002.判断文件夹是否存在.py
@@ -4,7 +4,9 @@ import os
 try:
 	#获得C:\Users\Administrator路径
 	home = os.path.expanduser("~")
+	#如果不存在C:\Users\Administrator\testdir
 	if not os.path.exists(home+'/testdir'):
+		#创建文件夹
 		os.makedirs(home+'/testdir')       
 except Exceptions as e:
     print e
