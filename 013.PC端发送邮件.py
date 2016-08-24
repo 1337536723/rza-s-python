573209822c42d264c5a483dd10f271314ddd11fc
RenZhiai <404261318@qq.com>
2016-08-24 22:41:26 +0800

    Update 013.PC端发送邮件.py

--- 013.PC端发送邮件.py
+++ 013.PC端发送邮件.py
@@ -1,34 +1,25 @@
 #coding:utf-8
-from email import encoders
-from email.header import Header
 from email.mime.text import MIMEText
-from email.utils import parseaddr, formataddr
-
 import smtplib
-import time
 
-def _format_addr(s):
-	name, addr = parseaddr(s)
-	return formataddr((Header(name, 'utf-8').encode(), addr))
+sender='rza0925@163.com'
+receiver='testrenzhiai@163.com'
+#邮件主题
+subject='python测试邮件'
+#发送邮箱服务器
+smtpserver='smtp.163.com'
+#发送方帐号密码
+username=sender
+password='rza24352248'
+	
+msg=MIMEText('你好！','plain','utf-8')
+msg['From']=sender
+msg['To']=receiver
+msg['Subject']=subject
 
-i=1
-while i<101:
-	from_addr="rza0925@163.com"
-	password="24352248110"
-	to_addr="testgmsapk@gmail.com"
-	smtp_server="smtp.163.com"
-	msg = MIMEText('hello, send by Python...'+str(i), 'plain', 'utf-8')
-
-	msg['From'] = _format_addr('周杰伦 <%s>' % from_addr)
-	msg['To'] = _format_addr('Python <%s>' % to_addr)
-	msg['Subject'] = Header('第'+str(i)+'封邮件', 'utf-8').encode()
-
-
-	server=smtplib.SMTP(smtp_server, 25)
-	#server.set_debuglevel(1)
-	server.login(from_addr, password)
-	server.sendmail(from_addr, [to_addr], msg.as_string())
-	server.quit()
-	print("发送第"+str(i)+"封邮件")
-	i=i+1
-	time.sleep(1)
+server=smtplib.SMTP()
+server.connect(smtpserver,'25')
+#server.set_debuglevel(1)
+server.login(username,password)
+server.sendmail(sender,receiver,str(msg))
+server.quit()
